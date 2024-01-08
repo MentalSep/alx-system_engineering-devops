9c400367b39ecf2acf5877fd591663b54ffa6aa8
MentalSep <chahidjava@hotmail.com>
2024-01-08 21:29:41 +0100

    add task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,9 @@
 # configures the web serevr
 
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => 'shell',
-  before   => Package['install nginx']
-}
-
-package { 'install nginx':
-  command  => 'sudo apt-get install -y nginx',
-  provider => 'shell',
-  before   => file_line['HTTP header']
-}
-
-file_line { 'HTTP header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => '        add_header X-Served-By $hostname;',
-  before => Service['nginx restart']
-}
-
-service { 'nginx restart':
-  command  => 'sudo service nginx restart',
-  provider => 'shell',
+exec { 'http header':
+  command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server {/a \ \ \ \ add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+  provider => shell,
 }
