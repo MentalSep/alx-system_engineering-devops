075b56da172455a58b47b5d97959a75c2fa849b5
MentalSep <chahidjava@hotmail.com>
2024-01-08 21:17:14 +0100

    add task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,7 +13,7 @@ file_line { 'HTTP header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => '        add_header X-Served-By \$hostname;'
+  line   => '        add_header X-Served-By $hostname;'
 }
 
 service { 'nginx':
