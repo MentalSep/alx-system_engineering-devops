df54779510b994914bea78b3bf821a5a2ea54b55
MentalSep <chahidjava@hotmail.com>
2024-01-08 20:45:21 +0100

    typo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,16 @@
 # configures the web serevr
 
-exec { 'apt-get update':
-  provider => 'shell',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
   ensure => 'installed',
-  require => Exec['apt-get update'],
+  require => Exec['update'],
 }
 
 file_line { 'HTTP header':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By $hostname;',
