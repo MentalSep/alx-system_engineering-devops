43edc82be96e1404b32d83f3ff24fd9caa715670
MentalSep <chahidjava@hotmail.com>
2024-02-13 11:07:02 +0100

    add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module wokring with Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+    else:
+        for post in response.json().get('data').get('children'):
+            print(post.get('data').get('title'))
