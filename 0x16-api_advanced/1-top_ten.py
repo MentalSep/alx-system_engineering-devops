d27cc3b143ea0cc6d42a68a679a2f8853c0dec0b
MentalSep <chahidjava@hotmail.com>
2024-02-13 11:09:11 +0100

    add task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code != 200:
         print(None)
     else:
